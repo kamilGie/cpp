34004b6fa3e75032ae96e5bb03945ccd5cba46c7
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-03-20 07:35:27 +0100

    zmiana krotkiego x1a

--- X1/X1a/krotki.cpp
+++ X1/X1a/krotki.cpp
@@ -1,3 +1,3 @@
-//kamil gieras
+//k g
 #include<iostream>
-int main(){int d,l,k;for(std::cin>>d>>l;std::cin>>k;l--)if(d%k)std::cout<<"NIE\n";else std::cout<<"TAK\n";}
+main(){int d,k;for(std::cin>>d>>k;std::cin>>k;)if(d%k)puts("NIE");else puts("TAK");}
