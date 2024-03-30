b0e36b237c877d1158dc19a6824c46f116b5c276
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-03-30 22:11:53 +0100

    dodaje testy wykonawcze

--- D2/src/main.cpp
+++ D2/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-//!\author to zadanie jest na tyle trudne ze jest zrobione o wiele profesjonalniej 
+//!\author to zadanie jest na tyle trudne ze bedzie zriobione  o wiele profesjonalniej 
 //!kazdy main jest w folderze testyWykonawcze w pliku testy  i sprawdzam czy dla kazdych tych warunkow program przechodzi 
 // tak sie pracuje w korporacjach  tdd to sie nazywa jak cos pozdro
 //!oddawanie  do bacy to  poprostu \include warehouse zmienie na caly plik i tyle 
---
+++ D2/testyWykonawcze/tests.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "../src/warehouse.hpp"
+
+TEST(test1, testing)
+{
+    
+}
