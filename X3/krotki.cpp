aed245bb44d948305f30613c8f749cb39842677c
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-04-05 20:01:02 +0200

    x3 x4 skrocienie

--- X3/krotki.cpp
+++ X3/krotki.cpp
@@ -3,14 +3,12 @@
 main(){
 int N,s,d=0;
 std::cin>>N>>s;
-unsigned short x[N];
+ushort x[N];
 
 for(;d<N;std::cin>>x[d++]);
 
 for(;std::cin>>s>>d;puts(""))
-for(bool r[65536]={};;s+=d){
+for(bool r[65536]{};;s+=d){
 s%=N;s<0?s+=N:0;
-if(r[x[s]])break;
-r[x[s]]=1;
-printf("%hu ",x[s]);}
-}
+if(r[x[s]]++)break;
+printf("%d ",x[s]);}}
--- X4/krotkie.cpp
+++ X4/krotkie.cpp
@@ -1,17 +1,17 @@
 //Kamil gieras
 #include<iostream>
 #include"tablica.h"
-#define t tablica
 main(){
-int N,s,L=65536,d,i=0;unsigned short*p=t+L;
+int N,s,L=65536,d,i=0;
+ushort*t=tablica,*p=t+L;
 for(std::cin>>N>>s;i++<N;std::cin>>*p++);
 
-for(;std::cin>>s>>d;puts("")){p=t;
+for(;std::cin>>s>>d;puts("")){
+p=t;
 for(i=0;i++<L;*p++=0);
 for(;;s+=d){
-  s%=N; s<0?s+=N:0;
-  if(*(t+*(t+s+L)))break;
-  *(t+*(t+s+L))=1;
-  printf("%d ",*(t+s+L));
+  s%=N;s<0?s+=N:0;
+  if((*(t+*t+s+L))++)break;
+  printf("%d ",*t+s+L);
 }
 }}
