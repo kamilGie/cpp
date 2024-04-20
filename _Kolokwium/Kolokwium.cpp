19bbd3ad3938e34c6721e41ce93e4c56d2d12b45
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-04-20 09:38:33 +0200

    programy do kolokwium

--- _Kolokwium/Kolokwium.cpp
+++ _Kolokwium/Kolokwium.cpp
@@ -6,13 +6,13 @@ int main(){
     /** stwórz przykładowe dane. TODO: wypełnij losowymi danymi? */
     int* data[] = { new int[25], new int[25], new int[20] };
     for (int i = 0; i < 3; ++i)
-        for (int j = 0; j < 20; ++j)
+        for (int j = 0; j < 20; ++j)//### te tablice czasami sa 25 elementowe a tutaj wypelniamy tylko pierwsze 20 elementow pozostale beda losowe
             data[i][j] = (i+1) * (j+1);
 
     // wybierz jedną z poprawnych tablic, nie dopuść, aby wybrano spoza zakresu
     int choice; do { 
         cout << "Wybierz tablice (1-3): "; cin >> choice; --choice; 
-    } while (choice < 0 || choice > 2);//### tutaj trzeba bylo zmienic whila bo byl zly zmieniam && na || oraz jak zmienilisy --choice to trzeba zmienic tez te wartosci o jeden 
+    } while (choice < 1 && choice > 3);//### tutaj trzeba bylo zmienic whila bo byl zly zmieniać && na || oraz jak zmienilisy --choice to trzeba zmienic tez te wartosci o jeden 
     int* p = data[choice];
 
     // niech użytkownik zgadnie liczbę 
@@ -22,13 +22,12 @@ int main(){
     // sprawdz czy liczba jest w tablicy
     int* l = p, *r = p + 25, *m;//### tam wczesniej mamy new int[20] wiec jedna  z tablic 20 elemtnowa jak dodamy to bedziemy sprawdzac za zakresem tej tablicy 
     while (l < r){
-        if (*(l++) == number) 
-            m = l;
-            cout<<*(l++)<<" "<<m<<endl;
+        if (*(l++) == number)
+            m = l;//### przypisujemy wartosic l do m jak l bedzie poprawne ale zwiekszy;ismy o jeden wiec przypisujemy nastepny element a nie ten co sie zgadzal 
     }
 
 
     // jeśli użytkownik trafił, to wypisz
-    if (*m == number) cout << "Trafiles!" << endl;//### nie zainicjowalismy tego m wiec moze byc ono poprawne bo bedzie miala losoe wartosci i losowo moze wylosowac numer ktory gracz podal 
+    if (*m == number) cout << "Trafiles!" << endl;//### nie zainicjowalismy tego m wiec jak uzytkoniwk nie zgadnie to tutaj niewiadomo co sie zdarzy (zadzialac, wyjebac program,niezadzialac xd wszystko )
     else cout << "Pudlo! Sprobuj jeszcze raz!" << endl;
 }
